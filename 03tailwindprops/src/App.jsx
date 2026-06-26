49278493441bfb707600724c499188b104ac0556
Syed Danish <syed.danish.in@gmail.com>
2026-06-26 12:14:48 +0400

    Implementation of props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,11 +8,18 @@ import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    username: 'Jolly',
+    age: 30,
+  }
+  let myArr = [1, 2, 3]
+
   return (
     //fragment
     <>
       <h1 className='bg-green-500 text-black p-4 rounded-xl '>Tailwind Test</h1>
-      <Card />
+      <Card username='Jane Smith' btnText='Get Started' />
+      <Card username='John Doe' btnText='Learn More' />
     </>
   )
 }
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function Card(props) {
-    console.log('props', props)
+function Card({ username, btnText }) {
+    console.log(username)
     return (
         <div className="max-w-xs rounded-md shadow-md bg-black text-gray-100">
             <img
@@ -11,7 +11,7 @@ function Card(props) {
             />
             <div className="flex flex-col justify-between p-6 space-y-8">
                 <div className="space-y-2">
-                    <h2 className="text-3xl font-semibold tracking-wide">Lorem</h2>
+                    <h2 className="text-3xl font-semibold tracking-wide">{username}</h2>
                     <p className="text-gray-400">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
                         tempora ipsum soluta amet corporis accusantium aliquid consectetur
@@ -22,7 +22,7 @@ function Card(props) {
                     type="button"
                     className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-gray-800 text-gray-200"
                 >
-                    Read more
+                    {btnText} 
                 </button>
             </div>
         </div>
